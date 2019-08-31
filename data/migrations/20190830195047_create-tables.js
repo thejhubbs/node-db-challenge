5db849051b1cae762cd7ab4f02d1309d7118388c
thejhubbs <thejhubbs@gmail.com>
2019-08-30 20:08:41 -0400

    Finished creating migrations

--- data/migrations/20190830195047_create-tables.js
+++ data/migrations/20190830195047_create-tables.js
@@ -2,49 +2,52 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('projects', tbl => {
-      tbl.increments('project_id');
+      tbl.increments('project_id'); //Id
+      tbl.text('project_name', 128)
+        .unique()
+        .notNullable(); //Name- unique & required
+      tbl.text('project_description'); //Description
+      tbl.boolean('project_completed'); //Boolean indicating completion
     })
     .createTable('tasks', tbl => {
-      tbl.increments('task_id');
+      tbl.increments('task_id'); //Id
+      tbl.text('task_description', 128)
+        .notNullable(); //Name- required
+      tbl.text('task_notes'); //Any other notes about the project.
+      tbl.boolean('task_completed'); //Boolean indicating completion
     })
     .createTable('resources', tbl => {
       tbl.increments('resource_id');
-    })
-
-    .createTable('ingredients', tbl => {
-      tbl.increments('ingredients_id');
-      tbl.text('ingredient_name', 128)
+      tbl.text('resource_name', 128)
         .unique()
-        .notNullable();
+        .notNullable(); //Name- unique & required
+      tbl.text('resource_description'); //Description
     })
-    .createTable('recipe_ingredients', tbl => {
-      tbl.integer('quanitity')
-        .unsigned()
-        .notNullable();
-
-      tbl.integer('recipe_id')
+    .createTable('project_resources', tbl => {
+      tbl.integer('resource_id')
         .unsigned()
         .notNullable()
-        .references('recipe_id')
-        .inTable('recipes')
+        .references('resource_id')
+        .inTable('resources')
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
-
-      tbl.integer('ingredient_id')
+      tbl.integer('project_id')
         .unsigned()
         .notNullable()
-        .references('ingredient_id')
-        .inTable('ingredients')
+        .references('project_id')
+        .inTable('projects')
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
+      tbl.primary(['resource_id', 'project_id']); //Many-to-many relationship table.
+    })
 
-      tbl.primary(['ingredient_id', 'recipe_id']);
-    });
+
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('recipes')
-    .dropTableIfExists('ingredients')
-    .dropTableIfExists('recipe_ingredients');
+    .dropTableIfExists('projects')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('project_resources');
 };
